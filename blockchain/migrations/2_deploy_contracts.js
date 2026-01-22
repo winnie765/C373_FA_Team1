ce55bc3852682794d7af36d6a363e8eb8390f37c
yuchen1234567 <zhengyuchen12@gmail.com>
2026-01-23 02:12:34 +0800

    Fix ganache connection (do a --reset to compile)

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 const TicketNFT = artifacts.require("TicketNFT");
 const LoyaltyToken = artifacts.require("LoyaltyToken");
 
-module.exports = function (deployer, network, accounts) {
+module.exports = function (deployer) {
   const gas = 6_500_000;
   deployer.deploy(TicketNFT, { gas });
   deployer.deploy(LoyaltyToken, { gas });
